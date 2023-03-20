da231f76626f0a761561a8c3af8a4ec5efbca72c
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-03-20 18:57:57 +0200

    test: Updated testing data

--- market-data-feeder/src/tests/mod.rs
+++ market-data-feeder/src/tests/mod.rs
@@ -5,15 +5,19 @@ pub mod integration_dev_network;
 
 const ORACLE_ADDRESS: &str = "nolus1436kxs0w2es6xlqpp9rd35e3d0cjnw4sv8j3a7483sgks29jqwgsv3wzl4";
 
-const NODE_CONFIG: &str = r#"json_rpc_protocol = "https"
+const NODE_CONFIG: &str = r#"#http2_concurrency_limit = 1
+json_rpc_protocol = "https"
 grpc_protocol = "https"
-host = "net-dev.nolus.io"
-json_rpc_port = 26612
+json_rpc_host = "net-dev.nolus.io"
+grpc_host = "net-dev.nolus.io"
+json_rpc_port = 26617
 # json_rpc_api_path = "/json-rpc/api"
-grpc_port = 26625
+grpc_port = 26620
 # grpc_api_path = "/grpc/api"
 address_prefix = "nolus"
 chain_id = "nolus-dev-v0-1-42"
-fee = { amount = "1500", denom = "unls" }
-gas_adjustment_numerator = 1015
-gas_adjustment_denominator = 1000"#;
+gas_adjustment_numerator = 105
+gas_adjustment_denominator = 100
+fee_denom = "unls"
+gas_price_numerator = 1
+gas_price_denominator = 390"#;
