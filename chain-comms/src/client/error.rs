0e79ae21a290397df4e99b0cb1cd81f27f7ef378
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-04-07 15:00:42 +0300

    chore: Updated dependencies

--- chain-comms/src/client/error.rs
+++ chain-comms/src/client/error.rs
@@ -5,7 +5,7 @@ pub enum Error {
     #[error("Failed to set up tendermint RPC client! Cause: {0}")]
     TendermintClient(#[from] cosmrs::rpc::Error),
     #[error("Failed to parse URI provided combination! Cause: {0}")]
-    InvalidUri(#[from] tonic::codegen::http::uri::InvalidUri),
+    InvalidUri(#[from] http::uri::InvalidUri),
     #[error("Failed to connect to node's RPC interface! Cause: {0}")]
     Connect(#[from] tonic::transport::Error),
 }
