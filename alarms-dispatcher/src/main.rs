69c1b1215282e59c397f3de3a9dabd11f3d412a6
Gancho Manev <g.manev@nolus.io>
2023-05-17 16:56:32 +0300

    chore(dispatcher): align the timealarms version

--- alarms-dispatcher/src/main.rs
+++ alarms-dispatcher/src/main.rs
@@ -26,7 +26,7 @@ pub mod error;
 pub mod messages;
 
 pub const ORACLE_COMPATIBLE_VERSION: SemVer = SemVer::new(0, 4, 3);
-pub const TIME_ALARMS_COMPATIBLE_VERSION: SemVer = SemVer::new(0, 3, 1);
+pub const TIME_ALARMS_COMPATIBLE_VERSION: SemVer = SemVer::new(0, 4, 0);
 
 pub const DEFAULT_COSMOS_HD_PATH: &str = "m/44'/118'/0'/0/0";
 
