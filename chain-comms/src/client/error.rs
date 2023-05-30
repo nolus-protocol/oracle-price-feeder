b80721c1841e6a66379592d706eaf1674531cc29
Kiril Mihaylov <k.mihaylov@nolus.io>
2023-05-30 16:19:34 +0300

    refactor: Changed error messages

--- chain-comms/src/client/error.rs
+++ chain-comms/src/client/error.rs
@@ -2,11 +2,11 @@ use thiserror::Error as ThisError;
 
 #[derive(Debug, ThisError)]
 pub enum Error {
-    #[error("Failed to set up tendermint RPC client! Cause: {0}")]
+    #[error("Failed to set up tendermint JSON-RPC client! Cause: {0}")]
     TendermintClient(#[from] cosmrs::rpc::Error),
-    #[error("Failed to parse URI provided combination! Cause: {0}")]
+    #[error("Failed to parse provided URI! Cause: {0}")]
     InvalidUri(#[from] http::uri::InvalidUri),
-    #[error("Failed to connect to node's RPC interface! Cause: {0}")]
+    #[error("Failed to connect to node's gRPC interface! Cause: {0}")]
     Connect(#[from] tonic::transport::Error),
 }
 
