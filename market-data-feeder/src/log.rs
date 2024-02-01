8299d273187d1f2d803cd337a4f82e2b7895da19
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2024-02-01 13:10:50 +0200

    refactor!(feeder): Use `broadcast`'s exported method for logging

--- market-data-feeder/src/log.rs
+++ market-data-feeder/src/log.rs
@@ -1,39 +1,12 @@
-use tracing::{debug, error, info, info_span};
+use tracing::{info, info_span};
 
-use chain_comms::{
-    interact::commit::Response,
-    reexport::cosmrs::tendermint::{abci::response::DeliverTx, Hash},
-};
+use chain_comms::reexport::cosmrs::tendermint::{abci::response::DeliverTx, Hash};
 
-pub fn commit_response(provider_id: &str, response: &Response) {
-    info_span!("Mempool Response", provider_id = provider_id).in_scope(|| {
-        info!("Hash: {}", response.hash);
-
-        if response.code.is_ok() {
-            debug!("Log: {}", response.log);
-        } else {
-            error!(
-                log = response.log,
-                "Error with code {} has occurred!",
-                response.code.value(),
-            );
-        }
-    });
-}
-
-pub fn tx_response(provider_id: &str, hash: &Hash, response: &DeliverTx) {
-    info_span!("Tx Response", provider_id = provider_id).in_scope(|| {
+pub fn tx_response(provider_name: &str, hash: &Hash, response: &DeliverTx) {
+    info_span!("Tx Response", provider_name = provider_name).in_scope(|| {
         info!("Hash: {}", hash);
 
-        if response.code.is_ok() {
-            debug!("Log: {}", response.log);
-        } else {
-            error!(
-                log = response.log,
-                "Error with code {} has occurred!",
-                response.code.value(),
-            );
-        }
+        broadcast::log::on_error(response.code, &response.log);
 
         info!("Gas limit for transacion: {}", response.gas_wanted);
         info!("Gas used: {}", response.gas_used);
