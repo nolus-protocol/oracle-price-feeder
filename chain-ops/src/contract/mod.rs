ee83e2b49fd3cb3a7ae5792a6afaf0003d0937f2
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2025-02-14 16:41:21 +0200

    refactor(chain-ops): Parse `SemVer` from semantic version string.

--- chain-ops/src/contract/mod.rs
+++ chain-ops/src/contract/mod.rs
@@ -1,3 +1,6 @@
+use std::{borrow::Cow, str::FromStr};
+
+use anyhow::Context as _;
 use serde::Deserialize;
 
 pub use self::admin::Admin;
@@ -5,6 +8,7 @@ pub use self::admin::Admin;
 pub mod admin;
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Deserialize)]
+#[serde(try_from = "Cow<'de, str>")]
 pub struct SemVer {
     major: VersionSegment,
     minor: VersionSegment,
@@ -39,6 +43,37 @@ impl SemVer {
         } else {
             Compatibility::Incompatible
         }
+    }
+}
+
+impl FromStr for SemVer {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.split_once('.')
+            .context("Version doesn't include a major version separator!")
+            .and_then(|(major, rest)| {
+                rest.split_once('.')
+                    .context(
+                        "Version doesn't include a major version separator!",
+                    )
+                    .and_then(|(minor, patch)| {
+                        Ok(Self {
+                            major: major.parse()?,
+                            minor: minor.parse()?,
+                            patch: patch.parse()?,
+                        })
+                    })
+            })
+    }
+}
+
+impl TryFrom<Cow<'_, str>> for SemVer {
+    type Error = anyhow::Error;
+
+    #[inline]
+    fn try_from(value: Cow<'_, str>) -> Result<Self, Self::Error> {
+        value.parse()
     }
 }
 
